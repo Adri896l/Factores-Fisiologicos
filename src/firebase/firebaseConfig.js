435b5c0bf364622ef862b557a442f497b7c08c42
Adriana Canseco <acc9908@gmail.com>
2025-04-21 20:43:28 -0600

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyANjKwn8s-LVoueQVMH2-3NL4l96nfeUr0",
-    authDomain: "encuesta-782fa.firebaseapp.com",
-    databaseURL: "https://encuesta-782fa-default-rtdb.firebaseio.com",
-    projectId: "encuesta-782fa",
-    storageBucket: "encuesta-782fa.firebasestorage.app",
-    messagingSenderId: "692300698679",
-    appId: "1:692300698679:web:bda96edbce7d48df721bb2"
+    apiKey: "------------------------------------",
+    authDomain: "---------------------------------",
+    databaseURL: "--------------------------------------",
+    projectId: "----------------------------------",
+    storageBucket: "---------------------------------",
+    messagingSenderId: "------------------------------",
+    appId: "------------------------------------------------"
 };
 
 // Inicializar Firebase
